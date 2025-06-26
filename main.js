0460b35f5f2577cf4be4c24b72cb369f02e57ba3
cumsud-ismayilov <c.smayilov1@gmail.com>
2025-06-26 19:27:34 +0400

    add

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ function ShowDetailModal(id) {
                     <p>${foundPizza.dsc}</p>
                     <p>Price:${foundPizza.price} AZN</p>
                     <p>Country:${foundPizza.country}</p>
-                    <div>${foundPizza.rate}</div>
+                    <div> ${generateRating(foundPizza.rate)} </div>
                     <button class="bg-[violet] p-[6px_20px] cursor-pointer"
                      ">Add To Cart</button>
 
